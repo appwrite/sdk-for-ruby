687a874b59e4f9b63e39e1b8f05367a2c7d91ba7
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-05-17 10:46:05 +0200

    feat: add response format for 0.14.0

--- lib/appwrite/client.rb
+++ lib/appwrite/client.rb
@@ -13,7 +13,7 @@ module Appwrite
             @headers = {
                 'user-agent' => RUBY_PLATFORM + ':ruby-' + RUBY_VERSION,
                 'x-sdk-version' => 'appwrite:ruby:5.0.0',                
-                'X-Appwrite-Response-Format' => '0.13.0'
+                'X-Appwrite-Response-Format' => '0.14.0'
             }
             @endpoint = 'https://HOSTNAME/v1'
         end

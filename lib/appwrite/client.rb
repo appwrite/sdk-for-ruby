c60b89040f023afdca65e252fb07a916e8c71a55
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-10 12:56:22 +0100

    fix: release

--- lib/appwrite/client.rb
+++ lib/appwrite/client.rb
@@ -15,10 +15,7 @@ module Appwrite
                 'x-sdk-name'=> 'Ruby',
                 'x-sdk-platform'=> 'server',
                 'x-sdk-language'=> 'ruby',
-                'x-sdk-version'=> '13.0.0',
-                'X-Appwrite-Response-Format' => '1.6.0'            
-            }
-
+                'x-sdk-version'=> '13.0.0',                'X-Appwrite-Response-Format' => '1.6.0'            }
             @endpoint = 'https://cloud.appwrite.io/v1'
         end
 
